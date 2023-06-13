42d2e6fb48a0bc9a48a632a33200fa3313960040
Oliver Einarsson <oliver.j.einarsson@gmail.com>
2023-06-12 18:28:24 -0700

    finishing updates to schema and added ERD

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -7,37 +7,37 @@ DROP TABLE IF EXISTS contacts;
 
 CREATE TABLE category(
 	category_id	varchar(10) PRIMARY KEY NOT NULL,
-	category varchar(30)
+	category varchar(30) NOT NULL
 );
 
 
 CREATE TABLE subcategory(
 	subcategory_id	varchar(10) PRIMARY KEY NOT NULL,
-	subcategory varchar(30)
+	subcategory varchar(30) NOT NULL
 );
 
 CREATE TABLE contacts(
 	contact_id SERIAL PRIMARY KEY NOT NULL,
-	first_name varchar(20),
-	last_name varchar(20),
-	email varchar(50)
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	email varchar(50) NOT NULL
 );
 
 CREATE TABLE campaign(
 	cf_id SERIAL PRIMARY KEY NOT NULL,	
-	contact_id	INT,
-	company_name varchar(50),
-	description	varchar(200),
-	goal NUMERIC(10, 2),
-	pledged	NUMERIC(10, 2),
+	contact_id	INT NOT NULL,
+	company_name varchar(50) NOT NULL,
+	description	varchar(200) NOT NULL,
+	goal NUMERIC(10, 2) NOT NULL,
+	pledged	NUMERIC(10, 2) NOT NULL,
 	outcome varchar(20) CHECK (outcome IN ('failed', 'successful', 'live', 'canceled')),
-	backers_count INT,	
-	country varchar(5),
-	currency varchar(5),
-	launch_date DATE,
-	end_date DATE,
-	category_id varchar(10),
-	subcategory_id varchar(30),
+	backers_count INT NOT NULL,	
+	country varchar(5) NOT NULL,
+	currency varchar(5) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id varchar(10) NOT NULL,
+	subcategory_id varchar(30) NOT NULL,
 	FOREIGN KEY (contact_id) REFERENCES contacts (contact_id),
 	FOREIGN KEY (category_id) REFERENCES category (category_id),
 	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
